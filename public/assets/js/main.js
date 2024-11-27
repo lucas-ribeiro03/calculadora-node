66ed1bd570e26dcfbab5e1973135f068094fb71e
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2024-11-26 22:21:48 -0300

    adicionando historico

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -68,17 +68,16 @@ document.querySelector('.btn-historico').addEventListener('click', () => {
     fetch('/historico').then(response => response.json()).then(data => {
         const historicoDiv = document.querySelector('.historico');
         historicoDiv.innerHTML = '';
+        historicoDiv.style.display = historicoDiv.style.display === 'none' ? 'block' : 'none'
 
         if(data.length > 0){
             const table = document.createElement('table');
-            const headerRow = document.createElement('tr');
-            headerRow.innerHTML = '<th>Conta</th><th>Resultado</th><th>Data</th>';
-            table.appendChild(headerRow);
+            table.classList.add('tabela-historico');
             data.forEach(conta => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-                <td>${conta.conta} = </td>
-                <td> ${conta.resultado} </td>
+                <td class="content">${conta.conta} = </td>
+                <td class="content"> ${conta.resultado} </td>
                 `;
                 table.appendChild(row);
             });
