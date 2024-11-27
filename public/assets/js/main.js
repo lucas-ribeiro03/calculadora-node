4381c344fcd594e2c6ba8b71a6d60def396dfce3
Lucas Ribeiro <lucasribeirollr05@gmail.com>
2024-11-26 22:20:55 -0300

    estilizando histórico

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -49,17 +49,42 @@ document.addEventListener('keydown', keyPressed => {
     }
     })
 
-    function salvarConta(conta, resultado){
-      fetch("/salvar-conta", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ conta, resultado }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Conta salva:", data);
-        })
-        .catch((e) => console.log(e, 'Nao funcionou'));
-    }
+function salvarConta(conta, resultado){
+    fetch("/salvar-conta", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ conta, resultado }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Conta salva:", data);
+    })
+    .catch((e) => console.log(e, 'Nao funcionou'));
+}
+
+document.querySelector('.btn-historico').addEventListener('click', () => {
+    fetch('/historico').then(response => response.json()).then(data => {
+        const historicoDiv = document.querySelector('.historico');
+        historicoDiv.innerHTML = '';
+
+        if(data.length > 0){
+            const table = document.createElement('table');
+            const headerRow = document.createElement('tr');
+            headerRow.innerHTML = '<th>Conta</th><th>Resultado</th><th>Data</th>';
+            table.appendChild(headerRow);
+            data.forEach(conta => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                <td>${conta.conta} = </td>
+                <td> ${conta.resultado} </td>
+                `;
+                table.appendChild(row);
+            });
+            historicoDiv.appendChild(table);
+        } else {
+            historicoDiv.innerHTML = '<p>Sem histórico para exibir.</p>'
+        }
+    }).catch(e => console.log('Erro ao carregar o histórico'))
+})
